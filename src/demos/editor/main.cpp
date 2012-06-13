d683f50197f7dd0387c0e0ca2960a30a69a5de5f
Don Boogert <don.boogert@gmail.com>
2012-06-13 08:27:32 +0100

    Fixed build of editor prman display driver for 3Delight

--- src/demos/editor/main.cpp
+++ src/demos/editor/main.cpp
@@ -7,6 +7,7 @@
 int main(int argc, char** argv)
 {
 	RiBegin("test.rib");
+	//RiDisplay("test", "ed", "rgba");
 	RiFormat(1024,768,1);
 	RiPixelSamples(3,3);
 
@@ -14,7 +15,6 @@ int main(int argc, char** argv)
 	RiSphere(1,-1,1,360);
 	RiWorldEnd();
 	RiEnd();
-
 
 	QApplication app(argc, argv);
 	app.setStyleSheet(" QListWidget { color: #B0B0B0; background-color: #505050; }\n QTextEdit { color: #B0B0B0; background-color: #303030; } \n QDockWidget { color: #B0B0B0; border-width: 0px; padding: 0px; margin:0px;} \n QDockWidget::title	 { color: #B0B0B0; text-align: right; background: #404040; }");
